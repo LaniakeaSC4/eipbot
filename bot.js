20ec23dc31908d56fe85c7110c1476b39c333f2b
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-09 22:43:13 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,26 @@ client.on('message', message => {
        }
 });
 
+if(message.content.startsWith("//inrole")){
+    let roleName = message.content.split(" ").slice(1).join(" ");
+
+    //Filtering the guild members only keeping those with the role
+    //Then mapping the filtered array to their usernames
+    let membersWithRole = message.guild.members.filter(member => { 
+        return member.roles.find("name", roleName);
+    }).map(member => {
+        return member.user.username;
+    })
+
+    let embed = new discord.RichEmbed({
+        "title": `Users with the ${roleName} role`,
+        "description": membersWithRole.join("\n"),
+        "color": 0xFFFF
+    });
+
+    return message.channel.send({embed});
+}
+
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
