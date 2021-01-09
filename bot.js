3b9eb0465bd14352c3260167e24d8842c24bdacd
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-09 16:11:32 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret the 
+client.login(process.env.kYkq3_-4jhFkITRXSBQz3ZGLdr2XIddY);//BOT_TOKEN is the Client Secret the 
