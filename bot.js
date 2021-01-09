95906584f409ca734bc67c54b4dc7eca1a46df92
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-09 17:00:03 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,8 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
 
@@ -19,11 +11,10 @@ client.on('message', message => {
        message.reply('pong');
 
        }
-
 });
-
- 
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
+
+//BOT_TOKEN is the Client Secret
