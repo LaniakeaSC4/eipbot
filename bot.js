54095583b65ddd9cb1b12e74a01595c7c88004b6
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-10 21:12:49 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -10,57 +10,51 @@ client.on('ready', () => {
 });
 
 //how long is teams
-var numofroles = teams.length;
-console.log(numofroles)
-
-//for each teams array in teams
-//for (const property in teams) {
-
-
+  //var numofroles = teams.length;
+  //console.log(numofroles)
 
 //start
 client.on('message', async message => {
-  
-  //pop the role stack
+
 
   //look for trigger
-    if (message.content.startsWith("!egg")) {
-      console.log('Seen an egg')
-      
-      var loops
-      for (loops = 0;loops < numofroles; loops++){
-      
-      const therole = teams.pop();
-console.log(therole) 
+      if (message.content.startsWith("!egg")) {
+       console.log('Seen an egg')
+       
+        //Set number of loops equal to number of roles
+        var loops
+        for (loops = 0;loops < numofroles; loops++){
+        
+       const therole = teams.pop();
+       //what role did we pop?
+         console.log(therole) 
+         
+       //debug counter. How many loops?
+          //console.log('one loop')
+        
+       //find role for this loop
+       const Role = message.guild.roles.cache.find(role => role.name == therole );
+        //fill members array with users in that role
+        const Members = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => member.user.tag);
 
-//debug counter
-console.log('one loop')
-
-      
-      
-      //find role
-      const Role = message.guild.roles.cache.find(role => role.name == therole );
-      //fill members array with users in that role
-      const Members = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => member.user.tag);
-
-        //basic output to channel (for troubleshooting)
-        //message.channel.send(`1. Users with ${Role.name}: ${Members}`);
+       //basic output to channel (for troubleshooting)
+         //message.channel.send(`1. Users with ${Role.name}: ${Members}`);
  
-        //Duplicate Members into Members2 so we can mess with Members 2 preserving Members
-        var Members2 = JSON.parse(JSON.stringify(Members)); 
-        //append \n to each arrary string so discord takes new line
-        for (var i=Members2.length; i--;) {Members2[i] = '\n' + Members2[i];} 
-        
-        //send message as output - broke Here? 
-        message.channel.send(`\nUsers with ${Role.name}: ${Members2}`);
-        
-        teams.unshift(therole)
-        
-    };
-};
-
-//end forEach
-});
+         //Duplicate Members into Members2 so we can mess with Members 2 preserving Members
+           var Members2 = JSON.parse(JSON.stringify(Members)); 
+          
+          //append \n to each arrary string so discord takes new line
+          for (var i=Members2.length; i--;) {Members2[i] = '\n' + Members2[i];} 
+          
+          //send message as output
+          message.channel.send(`\n Users with ${Role.name}: ${Members2}`);
+          
+         //Put role back in stack at the bottom
+          teams.unshift(therole)
+          
+     }; //end for loop
+  }; //end !egg trigger block
+}); //end 'on message'
 
 // THIS  MUST  BE  THIS  WAY
 
