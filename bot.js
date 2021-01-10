734d287677405a0a8e93b9bf234e736f97ede509
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-10 07:59:56 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,19 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on("message", message => {
 
+    if(message.content.startsWith(`${prefix}go4-add`)) {
+        message.mentions.members.first().addRole('415665311828803584'); // gets the <GuildMember> from a mention and then adds the role to that member                     
+    }
+
+    if(message.content == `${prefix}go4-list`) {
+        const ListEmbed = new Discord.RichEmbed()
+            .setTitle('Users with the go4 role:')
+            .setDescription(message.guild.roles.get('415665311828803584').members.map(m=>m.user.tag).join('\n'));
+        message.channel.send(ListEmbed);                    
+    }
+});
 
 // THIS  MUST  BE  THIS  WAY
 
