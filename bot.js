6bf629d974223dd211b1a7ecd9f0c8743d6a86d1
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-10 23:32:04 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -38,9 +38,9 @@ client.on('message', async message => {
           
              //rich embed
              const teamoutput = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            console.log(therole)
-            .setTitle('${therole}')
+            .setColor('#0099ff');
+            console.log(therole);
+            .setTitle(therole);
              
              //need to pop members here? Working here
              
