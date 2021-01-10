db814666d20253cb743d2188415fb6816d693fa3
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-10 19:38:53 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,9 @@ var numofroles = teams.length;
 console.log(numofroles)
 
 //for each teams array in teams
-for (const property in teams) {
+//for (const property in teams) {
+
+for (i = 0;i < teams.length; i++){
 
 //start
 client.on('message', async message => {
