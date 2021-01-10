ab42d84732bbd3fa075fc0f94f3f70566a8d02d1
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-10 19:17:05 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ console.log('one loop')
         for (var i=Members2.length; i--;) {Members2[i] = '\n' + Members2[i];} 
         
         //send message as output - broke Here? 
-        message.channel.send(`Users with ${Role.name}: ${Members2}`);
+        message.channel.send(`Users with ${Role.name}:\n ${Members2}`);
         
         teams.unshift(therole)
         
