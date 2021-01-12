f36ee38c75fe159d628bd4182d4b8c81c5112600
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-12 19:34:29 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-//build team array
-var teams = [ 'egg-streme' ];
-
 client.on('ready', () => {
 	//Are we alive?
 	console.log('I am ready!');
@@ -15,21 +12,17 @@ client.on('message', async message => {
 //look for !egg trigger
 if (message.content.startsWith("!egg")) {
 
-
-//pop a role to work with for this loop
-const therole = teams.pop();
-
 	let members = message.guild.members.cache.array();
-	let role = message.guild.roles.cache.find(r => r.name === 'Europe');
+	let role = message.guild.roles.cache.find(r => r.name === 'egg-streme');
 	
 	for(let member of members) {
    	let hasRole = member.roles.cache.has(role.id);
  if (hasRole === true){
-	   console.log(`${member.id}:apple ${hasRole}`)
+	   console.log(`${member.id}: is egg-streme`)
  } 
-else { console.log('banana')};
+else { console.log(`${member.id}: is not egg-streme`)};
 } 
-teams.unshift(therole);
+//teams.unshift(therole);
 
 }; //end !egg trigger block
 }); //end 'on message'
