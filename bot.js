73ebc3d879dce44758cfd35044ac75a8a9cfcf5b
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-12 21:42:47 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ thisteam = teams.pop();
 	for(let member of members) {
    	let hasRole = member.roles.cache.has(role.id);
  if (hasRole === true){
-	   userroles = message.member.roles.cache.map();
+	   userroles = member.roles.cache.map(r => '`'+r.name+'`').join(' - '), true);
 	   console.log(userroles);
 	   
 	   console.log(`${member.id}: is ` + thisteam)
