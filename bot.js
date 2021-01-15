ec125ef1397eb367b4989c2a8c0b25c259891f3c
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-15 20:30:08 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,49 +6,44 @@ client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-//check team function
-function checkteam(value) {
-	for (var i = 0; i < userroles.length; i++) {
-    	if (value.indexOf(userroles[i]) > -1) {
-      	return true;
-    		}
-  	}
-  	return false;
-	}
-
-//check time function
-function checktime(value) {
-	for (var i = 0; i < timezone.length; i++) {
-    	if (value.indexOf(timezone[i]) > -1) {
-      	return true;
-    		}
-  	}
-  	return false;
-	}
-
-//start
-client.on('message', async message => {
-
-//look for !egg trigger
-if (message.content.startsWith("!egg")) {
-
-//load up the members 
-let members = message.guild.members.cache.array();
-
 //Define the things we are going to want to sort by
 teams = ['egg-streme', 'yolksters','sunny-side','fowl-play','hard-boiled','over-easy'];
 timezone = ['Europe', 'US-WC', 'US-EC', 'South America', 'Oceania'];
 eggbonus = ['Medical Egger', 'Fusion Egger', 'Tachyon Egger', 'Antimatter Egger', 'Universe Egger', 'Enlightened Egger'];
 permitstatus = ['Pro Permit', 'Standard Permit'];
-	
-//This will be the top level arrary for each team
-var teamarray = [];
 
-//each object in teamarrary will be an arrary of the member display names
-var memberarray = [];
+//functions
+	//check team function
+	function checkteam(value) {
+		for (var i = 0; i < userroles.length; i++) {
+    		if (value.indexOf(userroles[i]) > -1) {
+      		return true;
+    			}
+  		}
+  		return false;
+		}
+
+	//check time function
+	function checktime(value) {
+		for (var i = 0; i < timezone.length; i++) {
+    		if (value.indexOf(timezone[i]) > -1) {
+      		return true;
+    			}
+  		}
+  		return false;
+		}
 
 //for each memberarray we will have another arrary containing the members details. Timezone, EB, Permit
 var memberdetails = [];
+
+//start
+client.on('message', async message => {
+
+//look for !egg trigger
+if (message.content.startsWith("!update")) {
+
+//load up the members 
+let members = message.guild.members.cache.array();
 	
 	//for every member on the server
 	for(let member of members) {
