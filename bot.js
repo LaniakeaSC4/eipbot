a5f3063038185c832ce17327196709dfce552e68
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-15 22:50:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ let members = message.guild.members.cache.array();
 
 //Add timezone
 timepick = timezone.filter(checktime);
-if (timepick.length == 0){timepick = "No Timezone"} else {
+if (!timepick.isArray(timepick) || !timepick.length)){timepick = "No Timezone"} else {
 memberdetails.splice(membuildcount,0, timepick);}		
 membuildcount = membuildcount + 1;
 		
