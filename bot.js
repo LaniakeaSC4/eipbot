144b5a27ca43c252ae9225d90b8d8114ef51cab2
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-20 19:53:41 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -277,8 +277,8 @@ client.on('message', async message => {
 			let messages = message.channel.messages.fetch().then(messages => {
        
         for (let message of messages){
-  		message.embeds.forEach((embed) => {console.log('embed title : ' + embed.title)});
-		//if(message.embeds.length > 0 ) {console.log('found the badger')} 
+  		console.log(message.embeds[0]);
+		console.log(message.embeds[0].length);
           } 
         }) 
        // const themessage = messages.filter(message.embed.Title.includes(String(eggcommand2))) ;
