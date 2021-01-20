9a6f2e0a86b797f9907bab838ace144bd7feb1aa
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-01-20 19:20:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -278,7 +278,7 @@ client.on('message', async message => {
        
         for (let message of messages){
           console.log(message);
-    if(message.embeds.length !== 0 && message.embed.Title.content.includes(String(eggcommand2))) {console.log('found the badger')
+    //if(message.embeds.length !== 0 && message.embed.Title.content.includes(String(eggcommand2))) {console.log('found the badger')
     } 
           } 
         }) 
