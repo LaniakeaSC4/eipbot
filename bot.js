11e5ea8519aaa5f9e39cc2f5f386260af2392545
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-08 22:39:11 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -277,9 +277,13 @@ client.on('message', async message => {
 			//look for embeded message fields matching the matching coop 
 			message.channel.messages.fetch({ limit: 100 })
   .then(fetched => {
-    const pinned = fetched.filter(fetchedMsg => fetchedMsg.pinned);});
+    const pinned = fetched.filter(fetchedMsg => fetchedMsg.pinned);
+    
+console.log(pinned);
+    
+  });
 			  
-			  console.log(pinned); 
+			   
 			  
         //message.channel.send(element.fields.find(Title => Title.value == eggcommand2)) ; //send a message containing the ID mentioned in 'args[0]' that was taken form the message
                 
