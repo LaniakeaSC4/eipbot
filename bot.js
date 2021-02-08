bf7bea5247ab183f85d41d948f596fd2eee39939
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-08 23:23:08 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -279,7 +279,7 @@ client.on('message', async message => {
   .then(fetched => {
     const cooppin = fetched.filter(fetchedMsg => fetchedMsg.pinned)
 
-    cooppin.edit(makecoopembed('#A822BD' , eggcommand2, 'edited coop', eggcommand2, id)); // edits message with newembed
+    fetchedMsg.edit(makecoopembed('#A822BD' , eggcommand2, 'edited coop', eggcommand2, id)); // edits message with newembed
    
   
 
