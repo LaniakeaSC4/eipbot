13bd793e4a42d08c5bde7ce603d5f0d0675cef4c
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-13 13:54:36 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -379,7 +379,7 @@ console.log('message: ' + message.id);
 
 //message.channel.messages.fetch(message).then(msg => {msg.reactions.resolve('👍').users.remove(thisid);}); 
 
-message.channel.messages.fetch(message.id).then(msg => {msg.reactions.cache.find(r => r.emoji.name == '👍').users.remove(thisid);}); 
+message.channel.messages.fetch(message.id).then(msg => {msg.reactions.resolve('👍').users.remove(thisid);}); 
 
 
 
