d6354047db149b99659eed0fe422bf34cfb876a8
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-13 13:01:59 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -377,8 +377,14 @@ var thisid = eggcommand2.substring(
 console.log('thisid: ' + thisid);
 console.log('message: ' + message);
 
-msg.channel.messages.fetchMessage(toString(message)).then(msg => {console.log('msg:' + msg);
- msg.reactions.resolve('👍').users.remove(thisid);});
+let messageEmbed = message.channel.fetchMessage(message)
+.catch(console.error);
+messageEmbed.reactions.first().remove(thisid)
+
+
+
+//msg.channel.messages.fetchMessage(toString(message)).then(msg => {console.log('msg:' + msg);
+ //msg.reactions.resolve('👍').users.remove(thisid);});
 
 
     
