e6e8d34fb12446eef63bfb5cfad2b02d727062e6
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-13 21:48:44 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -239,7 +239,7 @@ var coopname = "";
 async function removeuserreact(userid,messageid){
 
 //need to get message by I'd passed to function first
-message.channel.messages.fetch(messageid).then(msg => {
+channel.messages.cache.fetch(messageid).then(msg => {
 
 
 //then remove reaction
