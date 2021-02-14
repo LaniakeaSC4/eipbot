3b715febf3f75f890e152a5e898c6af678a75d9b
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-14 16:20:29 +0000

    update bot.js

--- bot.js
+++ bot.js
@@ -394,7 +394,7 @@ var thisuserid = eggcommand2.substring(
 console.log('thisid: ' + thisuserid);
 console.log('message: ' + message.id);
 
-message.reactions.cache.find(r => r.emoji.name == emoji).users.remove(thisuserid);
+m.reactions.cache.find(r => r.emoji.name == emoji).users.remove(thisuserid);
 
 
 //message.reactions.removeAll();
