8ec104940e01706ecfe45b3a428aa613e1f0b3db
LaniakeaSC4 <andy2004@gmail.com>
2021-02-15 21:53:44 +0000

    Embed.footer

--- bot.js
+++ bot.js
@@ -362,7 +362,7 @@ message.channel.messages.fetchPinned().then(messages => {
 		//for each pinned message
 		messages.forEach(message => {
 		  let embed = message.embeds[0];
-		  console.log(embed);
+		  console.log(embed.footer);
       if (embed && embed.footer.includes('LaniakeaSC')) {
         console.log('matched footer');
    message.edit(updatedEmbed); 
