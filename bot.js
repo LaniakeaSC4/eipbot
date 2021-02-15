8ac2efdf25fd871acbeaaad9b41a43a06aa51a74
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-15 13:31:18 +0000

    test with atom

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {console.log('I am ready!');});
 
+//test edit
+
 //===============================
 //	team update bot		|
 //===============================
@@ -26,7 +28,7 @@ var hardboiledarr = [];
 var overeasyarr = [];
 
 //functions
-//check functions: When looping through each member in update function, these functions look through the users roles (userroles) and return true for the role which matches. 
+//check functions: When looping through each member in update function, these functions look through the users roles (userroles) and return true for the role which matches.
 
 	//check team function
 	function checkteam(value) {
@@ -80,62 +82,62 @@ var overeasyarr = [];
 		}//end function
 
 	function update(message){
-  
+
 		//we are going to need a counter incremented for each match below so that we can order things into the array
 		var membuildcount = 0;
 		var memlistcount = 0;
-		
+
 		//clear arrays on start
 		memberdetails.splice(0,memberdetails.length);
 		memberlist.splice(0,memberlist.length);
-		
-		//load up the members 
+
+		//load up the members
 		let members = message.guild.members.cache.array();
-		
+
 		//for every member on the server
 		for(let member of members) {
-		
+
 			//start member details with display name. Splice it into the arrary at position membuildcount (0) counter then increment membuildcount so next object goes in next position. .
 			memberdetails.splice(membuildcount,0, member.displayName);
 			membuildcount = membuildcount + 1;
-		
+
 			//now lets get all thier roles into a string
 			userroles = member.roles.cache.map(r => r.name);
-			
+
 			//Add timezone using checktime function.
 			//fill timepick with objects from the timezone list which pass checktime as true (should just be one match). Splice it into the next location of memberdetails then increment build count.
 			timepick = timezone.filter(checktime);
 			if (timepick.length == 0)
-			{memberdetails.splice(membuildcount,0, "No Timezone")} 
-			else {memberdetails.splice(membuildcount,0, String(timepick))}		
+			{memberdetails.splice(membuildcount,0, "No Timezone")}
+			else {memberdetails.splice(membuildcount,0, String(timepick))}
 			membuildcount = membuildcount + 1;
-			
+
 			//Add team with checkteam function (see timepick)
 			teampick = teams.filter(checkteam);
 			if (teampick.length == 0)
 			{memberdetails.splice(membuildcount,0, "No Team");}
 			else {memberdetails.splice(membuildcount,0, String(teampick));}
 			membuildcount = membuildcount + 1;
-			
+
 			//Add egg bonus with checkbonus function (see timepick)
 			bonuspick = eggbonus.filter(checkbonus);
 			if (bonuspick.length == 0)
 			{memberdetails.splice(membuildcount,0, "No Bonus");}
 			else {memberdetails.splice(membuildcount,0, String(bonuspick));}
 			membuildcount = membuildcount + 1;
-			
+
 			//Add permit status with checkpermit function (see timepick)
 			permitpick = permitstatus.filter(checkpermit);
 			if (permitpick.length == 0)
 			{memberdetails.splice(membuildcount,0, "No Permit Status");}
 			else {memberdetails.splice(membuildcount,0, String(permitpick));}
 			membuildcount = membuildcount + 1;
-		
-			//now we have all user details in memberdetails [0] to [4], splice all that as one object into memberlist. 
+
+			//now we have all user details in memberdetails [0] to [4], splice all that as one object into memberlist.
 			memberlist.splice(memlistcount,0, memberdetails);
 			memlistcount = membuildcount + 1;//increment memlistcount ready for next loop/user
 			membuildcount = 0;//reset membuild count
-			memberdetails = [];//this may not be needed. Try take it out? 
+			memberdetails = [];//this may not be needed. Try take it out?
 		}//end for (let member of members) - every member on the server
 
 		//build the team specific arrays
@@ -155,7 +157,7 @@ var overeasyarr = [];
 	.setTitle(String(title))
 	.setDescription(String(description));
 
-	//loop to add the team members to the rich embed. Team specific array contains object for each member, so we loop through them to ad fields to the embed. 
+	//loop to add the team members to the rich embed. Team specific array contains object for each member, so we loop through them to ad fields to the embed.
 	for (var i = 0; i < array.length; i++) {
 		TeamEmbed.addFields(
 			{ name: array[i][0], value: 'Rank: ' + array[i][3] + '\n' + 'Time Zone: ' + array[i][1] + '\n' + 'Permit: ' + array[i][4], inline: true},
@@ -172,7 +174,7 @@ client.on('message', async message => {
 
 	//look for !update trigger. Allows manual update. Not needed with team outputs as update function is called first in those anyway.
 	if (message.content.startsWith("!update")) {
-		//updates all the arrays, passing message details to the function. 
+		//updates all the arrays, passing message details to the function.
 		update(message);
 		message.channel.send("Teams updated");
 		message.delete();
@@ -206,7 +208,7 @@ client.on('message', async message => {
 		if (message.content.startsWith("!hardboiled")) {
 		update(message);
 		message.channel.send(prepupdate('#5C90E1' , 'Hard Boiled', 'Here are the team members in team Hard Boiled!', hardboiledarr));
-		}//end !hardboiled trigger block 
+		}//end !hardboiled trigger block
 
 	//look for !overeasy trigger
 		if (message.content.startsWith("!overeasy")) {
@@ -246,13 +248,13 @@ messages.fetch(messageid).then(msg => {
 msg.reactions.resolve('👍').users.remove(userid);
 
 
-}) 
-} 
+})
+}
 
 //!coop
 client.on('message', async message => {
 	if (message.content.startsWith("!coop")){
-		
+
 		//first lets split up commands
 		//transfer message contents into msg
 		let msg = message.content;
@@ -262,19 +264,19 @@ client.on('message', async message => {
 		let argArr = argString.split(' ');
 		//for each element in array, make into variable
 		let [eggcommand1, eggcommand2, eggcommand3] = argArr;
-		
+
 		console.log('commmand 1 is: ' + eggcommand1);
 		console.log('commmand 2 is: ' + eggcommand2);
 		console.log('commmand 3 is: ' + eggcommand3);
-		
+
 		//start a coop
 		//if first thing after !coop is "start" take 2nd thing as coop name
 		if (eggcommand1 == 'start' && String(eggcommand2) !== "undefined"){
-		  
-		  
+
+
 		  //unpin all messages
-		  message.channel.messages.fetchPinned().then(messages => {messages.forEach(message => { message.unpin()})}); 
-		  
+		  message.channel.messages.fetchPinned().then(messages => {messages.forEach(message => { message.unpin()})});
+
 			//post the inital coop embed post then edit it to add the message ID (might not need the ID?)
 			message.channel.send(makecoopembed('#A822BD' , eggcommand2, 'New coop', eggcommand2, 'No ID' )).then(sent => {
 		  	let id = sent.id;
@@ -284,13 +286,13 @@ client.on('message', async message => {
 			sent.pin();
 			});//end the .then part
 		};//end the if "start" block
-  		
+
 //=========
 
 if (eggcommand1 == 'open' && String(eggcommand2) !== "undefined"){
 
 //unpin all messages
-message.channel.messages.fetchPinned().then(messages => {messages.forEach(message => { message.unpin()})}); 
+message.channel.messages.fetchPinned().then(messages => {messages.forEach(message => { message.unpin()})});
 
 //build initial message
 let embed = new Discord.MessageEmbed()
@@ -371,33 +373,33 @@ await msg.edit(newEmbed);
 	  }
 	  //-=--=-=-=-=-=-=
   });
-			  
-			  
-			  
-			  
+
+
+
+
 			});//end the .then part
 		};//end the if "open" block
 
 //=========
 		//!coop placed @user
 		if (eggcommand1 == "placed" && String(eggcommand2) !== "undefined"){
-			//look for embeded message fields matching the matching coop 
+			//look for embeded message fields matching the matching coop
 	message.channel.messages.fetchPinned().then(messages => {console.log(`Received ${messages.size} messages`);
 
 var testuserid = message.mentions.users.first().id;
 console.log(testuserid);
-	
+
   //Iterate through the messages here with the variable "messages".
   messages.forEach(message => {
-    
+
 console.log('message ID:' + message.id);
 console.log('eggcommand2:' + eggcommand2);
 
 //get user Id of who was @mentioned
 var thisuserid = eggcommand2.substring(
-    eggcommand2.lastIndexOf("@") + 1, 
+    eggcommand2.lastIndexOf("@") + 1,
     eggcommand2.lastIndexOf(">")
-); 
+);
 
 console.log('thisid: ' + thisuserid);
 console.log('message: ' + message.id);
@@ -408,12 +410,12 @@ console.log('message: ' + message.id);
 //message.reactions.removeAll();
 votes[userReaction.emoji.name].delete(user);
 
-  }) 
+  })
 });
 
 };//end if farming block
 	} ;//end if !coop block
-  
+
 //add and pin post (use function to make post).
 
 
@@ -424,6 +426,6 @@ votes[userReaction.emoji.name].delete(user);
 
 client.login(process.env.BOT_TOKEN);
 
-//BOT_TOKEN is the Client Secret 
+//BOT_TOKEN is the Client Secret
 
 //&& message.embeds[0].Title.content.includes(String(eggcommand2))
