11c6cc87452e28e07d19b078e59762fd607d0c39
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-02-17 14:17:55 +0000

    defined variables outside function

--- bot.js
+++ bot.js
@@ -241,16 +241,23 @@ client.on('message', async message => {
 //open a new coop
 if (eggcommand1 == 'open' && String(eggcommand2) !== "undefined"){
 
-async function initalisefarmers() {
+	var eggstremeMem = new Array();
+	var overeasyMem =  new Array();
+	var yolkstersMem = new Array();
+	var sunnysideMem = new Array();
+	var fowlplayMem = new Array();
+	var hardboiledMem = new Array();
+
+function initalisefarmers() {
 	//build initial team arrays. These arrarys will be looped through and updated when we place a member in a coop
 	//we are building them here becuase we dont want to rebuild the arrary on each command/message. Could make this into a functin to allow manual updating, but should be ok for now
 	//If we update these master arrary...it might mess up if we have multiple coops. We may want to copy them into the context of the message/channel or whatever before editing
-	var eggstremeMem = message.guild.roles.cache.get('717392493682884648').members.map(m=>m.user.username).join("\n");
-	var overeasyMem =  message.guild.roles.cache.get('717392318017175643').members.map(m=>m.user.username).join("\n");
-	var yolkstersMem = message.guild.roles.cache.get('717391863287644251').members.map(m=>m.user.username).join("\n");
-	var sunnysideMem = message.guild.roles.cache.get('717392245761900545').members.map(m=>m.user.username).join("\n");
-	var fowlplayMem = message.guild.roles.cache.get('717392169861644339').members.map(m=>m.user.username).join("\n");
-	var hardboiledMem = message.guild.roles.cache.get('717392100043390977').members.map(m=>m.user.username).join("\n");
+	eggstremeMem = message.guild.roles.cache.get('717392493682884648').members.map(m=>m.user.username).join("\n");
+	overeasyMem =  message.guild.roles.cache.get('717392318017175643').members.map(m=>m.user.username).join("\n");
+	yolkstersMem = message.guild.roles.cache.get('717391863287644251').members.map(m=>m.user.username).join("\n");
+	sunnysideMem = message.guild.roles.cache.get('717392245761900545').members.map(m=>m.user.username).join("\n");
+	fowlplayMem = message.guild.roles.cache.get('717392169861644339').members.map(m=>m.user.username).join("\n");
+	hardboiledMem = message.guild.roles.cache.get('717392100043390977').members.map(m=>m.user.username).join("\n");
 };
 
 function addsq() {
