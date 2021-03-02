4d1bf248d1c72233e3b2aff813e217fc4cddfd70
LaniakeaSC4 <andy2004@gmail.com>
2021-03-02 08:27:07 +0000

    Logging

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', async message => {
 	if (message.content.startsWith("!build")) {
 
 		buildteamarrays(message);
-		//console.log(teammembers)
+		console.log(teammembers)
 		//console.log('teams:' + teams)
 	}
 });//end client on message
