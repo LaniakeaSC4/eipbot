fb50e7ae32948e614ab4360a16d1fb0b8919eb74
LaniakeaSC4 <andy2004@gmail.com>
2021-03-03 10:22:54 +0000

    Stsus channel send

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => { console.log('I am ready!'); });
+client.on('ready', () => { 
+  console.log('I am ready!'); 
+  var statuschannel = bot.channels.cache.find(channel => channel.name === "bot-status"); 
+   client.channels.cache.get(statuschannel)
+          .send(`EIP Bot online!`);
+});
 
 // ---- Info ----
 // home team should be under category including word "home"
