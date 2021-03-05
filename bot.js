085971e190929bf431490fc2bfee9de7b5386d46
LaniakeaSC4 <andy2004@gmail.com>
2021-03-05 23:47:37 +0000

    455

--- bot.js
+++ bot.js
@@ -386,7 +386,7 @@ function restartcollector(message) {
 					return new Promise((resolve, reject) => {
 						 
 						// raw.js event file
-						ureacts = message.reactions.forEach(r => {
+						ureacts = message.reactions.cache.forEach(r => {
 							r.fetchUsers({before: `${reaction.message.author.id}`});
 						});
 						resolve(ureacts)
