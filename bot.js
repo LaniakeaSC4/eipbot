8f1a1c5f06e62f46d64aabc2afe6d41a923baa6d
LaniakeaSC4 <77202101+LaniakeaSC4@users.noreply.github.com>
2021-03-06 21:35:56 +0000

    fetch user

--- bot.js
+++ bot.js
@@ -391,6 +391,8 @@ function restartcollector(message) {
 					for (var k = 0; k < collectorstate.farming.length; k++) {
 						console.log('found 1 thumbs up')
 						console.log(collectorstate.farming[k])
+						var thisuser = client.users.fetch(collectorstate.farming[k])
+						console.log(thisuser)
 					}
 				}
 
