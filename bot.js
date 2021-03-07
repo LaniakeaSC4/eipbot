3654da1d270d6a533f1ff777d38805febf2cdbfd
LaniakeaSC4 <andy2004@gmail.com>
2021-03-07 08:50:36 +0000

    Wrong await

--- bot.js
+++ bot.js
@@ -440,7 +440,7 @@ function clearboard(message) {
 						);
 
 						//edit message with newEmbed to update it
-						await msg.edit(newEmbed);
+						msg.edit(newEmbed);
 				  
 				}
 				reolve()
@@ -490,7 +490,7 @@ function restartvotes(message) {
 						);
 
 						//edit message with newEmbed to update it
-						msg.edit(newEmbed);
+					await	msg.edit(newEmbed);
 						//console.log(newvotes);
 					}
 
