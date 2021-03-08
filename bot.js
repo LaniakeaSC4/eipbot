2608d96b016078eadff233b2192539076486a4b3
LaniakeaSC4 <andy2004@gmail.com>
2021-03-08 18:29:39 +0000

    Added partials and reaction logging

--- bot.js
+++ bot.js
@@ -1,5 +1,26 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+client.on('messageReactionAdd', async (reaction, user) => {
+	// When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+			console.log('a partial reaction was fetched')
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+	// Now the message has been cached and is fully available
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+
+
+
 client.on('ready', () => {
 	console.log('I am ready!');
 	//var statuschannel = client.channels.cache.find(channel => channel.name === "bot-status");
