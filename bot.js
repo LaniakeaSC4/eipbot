7f51c3de61a0af573d7903f46770a4ed08d05ab0
LaniakeaSC4 <andy2004@gmail.com>
2021-03-09 07:47:27 +0000

    Log reaction.users

--- bot.js
+++ bot.js
@@ -36,6 +36,8 @@ client.on('messageReactionAdd', async (reaction, user) => {
 		}
 	}
 
+console.log('users: ' + reaction.users)
+
 	//when reaction is added, check the ID of the message it was added to. If it matches one of the open status boards then...
 	for (var i = 0; i < statusboardmessages.length; i++) {
 		if (statusboardmessages[i].includes(reaction.message.id)) {
