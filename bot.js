340dd12a5900f4a21b6689c01126776686059c41
LaniakeaSC4 <andy2004@gmail.com>
2021-03-19 19:37:01 +0000

    Moved reaction remove code

--- bot.js
+++ bot.js
@@ -163,11 +163,11 @@ client.on('messageReactionAdd', async (reaction, user) => {
 				if (thisuser != "EiP Bot" && allowedemoji.includes(reaction.emoji.name)) {
 					//get the message object for the status board which recieved the reaction, then...
 					await client.channels.cache.get(thischannel).messages.fetch(thismessage).then(async msg => {
-						try {
-						  reaction.message.reactions.cache.find(reaction => reaction.emoji.name == "👍").users.remove(user.id);
+					  reaction.message.reactions.cache.find(reaction => reaction.emoji.name == "👍").users.remove(user.id);
 						  reaction.message.reactions.cache.find(reaction => reaction.emoji.name == "👎").users.remove(user.id);
 						  reaction.message.reactions.cache.find(reaction => reaction.emoji.name == "🥚").users.remove(user.id);
-						  reaction.message.reactions.cache.find(reaction => reaction.emoji.name == "💤").users.remove(user.id);
+						  reaction.message.reactions.cache.find(reaction => reaction.emoji.name == "💤").users.remove(user.id); 
+						try {
 							//reaction.message.reactions.removeAll()//remove all reactions to prevent extra input
 							await rebuildteamobj(msg)//rebuild the teammembers object for *this* status board
 							await changeplayerstatus(reaction.emoji.name, thisuser)//update the user in the teammembers object with the new emojj
