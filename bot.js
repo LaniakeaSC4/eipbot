d1786d5e93c423881dbd1eeedf132d41a590ccb3
LaniakeaSC4 <andy2004@gmail.com>
2021-03-26 23:30:18 +0000

    Coop id

--- bot.js
+++ bot.js
@@ -20,8 +20,7 @@ var coopname = coopdetails[0]
 var coopid = coopdetails[1]
 var thiscoop = message.content.substr(0,message. content.indexOf(","))
 var embed = new Discord.MessageEmbed
-embed.setDescription("e!coop " + coopid + " " + thiscoop)
-embed.addField("Click to view this contract on mioi.io", "https://egginc.mioi.io/contract/view/"+coopname+"/"+thiscoop+"/")
+embed.addField("Click to view this contract on mioi.io", "https://egginc.mioi.io/contract/view/"+coopid+"/"+thiscoop+"/")
 message.channel.send("Hey there... I couldn't help but notice you posted an update for " + coopname + ". Thanks for doing that. If anyone else wanted to ask Egg, Inc Bot for an update for this coop they could do it with the command below. Remember, only DM the bot... We don't want our coop names to leak", embed)
 	}
 });//end client on message 
