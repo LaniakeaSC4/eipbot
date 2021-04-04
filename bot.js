467fb69f22da74b954d22351108d38ca1d6ef1b6
LaniakeaSC4 <andy2004@gmail.com>
2021-04-04 13:12:11 +0100

    Substring

--- bot.js
+++ bot.js
@@ -490,7 +490,7 @@ console.log('playerid is: ' + playerid)
 		await rebuildteamobj(message)
 		let user = await hexsearch(playerid)
 		console.log('user is: ' + user)
-		var trimuser = user.substr(user.indexOf("-") + 1,user.indexOf('(') - 1)
+		var trimuser = user.substring(user.indexOf("-") + 1,user.indexOf('(') - 1)
 		console.log('user is now: ' + trimuser)
 		await changeusersquare(oldsq1, oldsq2, newsq, trimuser)//change squares in the memory object
 		await updateplayerboard(message, source)//update player board from memory object
