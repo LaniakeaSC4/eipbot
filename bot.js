01ea46f34b29f413e2f90b8ca5b8427b6fbc2a3e
LaniakeaSC4 <andy2004@gmail.com>
2021-04-04 12:46:56 +0100

    User crop

--- bot.js
+++ bot.js
@@ -490,6 +490,8 @@ console.log('playerid is: ' + playerid)
 		await rebuildteamobj(message)
 		let user = await hexsearch(playerid)
 		console.log('user is: ' + user)
+		user = user.substr(indexOf("-" + 1),indexOf('(' - 1))
+		console.log('user is now: ' + user)
 		await changeusersquare(oldsq1, oldsq2, newsq, user)//change squares in the memory object
 		await updateplayerboard(message, source)//update player board from memory object
 		console.log('user ' + user + ' finished being updated to ' + newsq)
