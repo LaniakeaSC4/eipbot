73eb8b66aaf131e34f00e43054deb8d2ae62af40
LaniakeaSC4 <andy2004@gmail.com>
2021-07-26 19:50:22 +0100

    Add log

--- bot.js
+++ bot.js
@@ -649,7 +649,8 @@ console.log(master[message.guild.id].teammembers)
 			//add teams and players for embed from teams/teammeber objects
 			for (var i = 0; i < master[message.guild.id].teams.length; i++) {
 				var cleanrole = master[message.guild.id].teams[i].replace(/[^a-zA-Z ]/g, "");//teammebers object is keyed with a cleaned version of role (no hyphen). Uncleaned roles are in teams object
-				
+				console.log(master[message.guild.id].teams[i])
+				console.log(master[message.guild.id].teammembers[cleanrole])
 				placedEmbed.addField(`Team ${master[message.guild.id].teams[i]}`, master[message.guild.id].teammembers[cleanrole], false)
 			}//end loop to add team fields to embed
 
